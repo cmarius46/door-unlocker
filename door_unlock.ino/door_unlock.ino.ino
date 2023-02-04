e607009c710b4a54e4ba44db0e7749d6e414cd0e
cmarius46 <cmarius46@gmail.com>
2023-02-04 18:23:01 +0200

    ino

---
+++ door_unlock.ino/door_unlock.ino.ino
@@ -0,0 +1,23 @@
+#include "Servo.h"
+
+#define micPin 7
+#define fingerPin 6
+
+
+Servo finger;
+
+int val;
+
+void setup() {
+  // put your setup code here, to run once:
+  // servos;
+  finger.attach(fingerPin);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(micPin);
+  Serial.println(val);
+  delay(50);
+}
